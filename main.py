67157db8a0af2081801899cfaa5f601483c1f0e9
Tara Pande <tarande57@gmail.com>
2026-05-22 17:13:02 -0700

    data analysis and linear model done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.pharma_adherence.data import PharmaDataset
-#TODO: Import ModelTrainer class from modeling.py
+from src.pharma_adherence.modeling import ModelTrainer
 
 """
 DAY 1: DATA PREPROCESSING & ANALYSIS
@@ -9,6 +9,7 @@ dataset = PharmaDataset("data/raw/prescriptions_large_raw.csv")
 print(dataset.df.head())
 
 #TODO: Clean the dataset
+dataset.clean()
 
 #TODO: Save the dataset as a csv into "data/processed/"
 dataset.save("data/processed/prescriptions_large_cleaned.csv")
@@ -22,10 +23,13 @@ DAY 2: MACHINE LEARNING
 """
 
 #TODO: Instantiate a linear regression trainer
+linear_model = ModelTrainer(dataset.df, "proportion_days_covered", ["sex", "copay_amount"])
 
 #TODO: Train the linear model
+model, metrics = linear_model.train_linear()
 
 #TODO: Print the linear model metrics
+print(metrics)
 
 #TODO: Instantiate a logistic regression trainer
 
